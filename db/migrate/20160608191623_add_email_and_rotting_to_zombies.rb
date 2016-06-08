ee1c7bd02b32d771341bfd52c7a7fdfb55978fb6
Peter Lollo <peteralollo@gmail.com>
2016-06-08 13:48:58 -0700

    Generate user scaffold

--- db/migrate/20160608191623_add_email_and_rotting_to_zombies.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailAndRottingToZombies < ActiveRecord::Migration
-  def change
-    add_column :zombies, :email, :string
-    add_column :zombies, :rotting, :boolean, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608200156) do
+ActiveRecord::Schema.define(version: 20160608204039) do
 
-  create_table "zombies", force: :cascade do |t|
-    t.string  "name"
-    t.text    "bio"
-    t.integer "age"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "payments",   default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
