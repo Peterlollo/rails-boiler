0af8336255d4449f46cc1a3012c999bb979e1c58
Peter Lollo <peteralollo@gmail.com>
2016-06-08 13:28:58 -0700

    Remove duplicate migration

--- db/migrate/20160608200156_create_zombies_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateZombiesTable < ActiveRecord::Migration
-  def change
-    create_table :zombies do |t|
-      t.string :name
-      t.text :bio
-      t.integer :age
-    end
-  end
-end
